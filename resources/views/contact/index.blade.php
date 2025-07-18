5f94d40335afcfc610ef5e7d8638149fc92dba08
Emerson <emerson.f.f@outlook.com>
2025-07-18 20:32:43 -0300

    added pagination

--- resources/views/contact/index.blade.php
+++ resources/views/contact/index.blade.php
@@ -34,37 +34,39 @@
                     <tbody>
                         @foreach ($contacts as $contactInformation)
                             <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 border-gray-200">
-                                <td class="p-0">
-                                    <a class="w-full h-full block p-4" href="{{ route('contacts.show', $contactInformation) }}">
-                                        {{$contactInformation->name}}
-                                    </a>
+                                <td class="p-4">
+                                    {{$contactInformation->name}}
                                 </td>
-                                <td class="p-0">
-                                    <a class="w-full h-full block p-4" href="{{ route('contacts.show', $contactInformation) }}">
-                                        {{$contactInformation->contact}}
-                                    </a>
+                                <td class="p-4">
+                                    {{$contactInformation->contact}}
                                 </td>
-                                <td class="p-0">
-                                    <a class="w-full h-full block p-4" href="{{ route('contacts.show', $contactInformation) }}">
-                                        {{$contactInformation->email}}
-                                    </a>
+                                <td class="p-4">
+                                    {{$contactInformation->email}}
                                 </td>
 
                                 @if($isAuthenticated)
-                                    <th scope="col" class="px-6 py-3">
-                                        <form action="{{route('contacts.destroy', $contactInformation)}}" method="POST">
-                                            @method('delete')
-                                            @csrf
-                                            <x-danger-button class="p-0">
-                                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-trash2-icon lucide-trash-2"><path d="M10 11v6"/><path d="M14 11v6"/><path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6"/><path d="M3 6h18"/><path d="M8 6V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2"/></svg>
-                                            </x-danger-button>
-                                        </form>
+                                    <th scope="col" class="p-0">
+                                        <div class="flex gap-4">
+                                            <a class="inline-flex items-center p-2 bg-white dark:bg-gray-800 border border-gray-300 dark:border-gray-500 rounded-md font-semibold text-xs text-gray-700 dark:text-gray-300 uppercase tracking-widest shadow-sm hover:bg-gray-50 dark:hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 dark:focus:ring-offset-gray-800 disabled:opacity-25 transition ease-in-out duration-150" href="{{ route('contacts.show', $contactInformation) }}">
+                                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-eye-icon lucide-eye"><path d="M2.062 12.348a1 1 0 0 1 0-.696 10.75 10.75 0 0 1 19.876 0 1 1 0 0 1 0 .696 10.75 10.75 0 0 1-19.876 0"/><circle cx="12" cy="12" r="3"/></svg>
+                                            </a>
+                                            <form action="{{route('contacts.destroy', $contactInformation)}}" method="POST">
+                                                @method('delete')
+                                                @csrf
+                                                <x-danger-button>
+                                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-trash2-icon lucide-trash-2"><path d="M10 11v6"/><path d="M14 11v6"/><path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6"/><path d="M3 6h18"/><path d="M8 6V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2"/></svg>
+                                                </x-danger-button>
+                                            </form>
+                                        </div>
                                     </th>
                                 @endif
                             </tr>
                         @endforeach
                     </tbody>
                 </table>
+                <div class="mt-2">
+                {{ $contacts->links() }}
+                </div>
             </div>
         </div>
     </div>
