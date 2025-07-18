1976c4ffda5629ca10c1d6470c5c08d51f8a7968
Emerson <emerson.f.f@outlook.com>
2025-07-18 19:56:07 -0300

    removing mention to welcome and replacing with a call to controller@index

--- routes/web.php
+++ routes/web.php
@@ -14,10 +14,7 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
-
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [ContactController::class, 'index'])->name('home');
 
 Route::get('/contacts', [ ContactController::class,'index'])->name('contacts.index');
 
