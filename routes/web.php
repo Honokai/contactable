d1181c8e7429c5ecbc89e5a50c19c4f265015d8a
Emerson <emerson.f.f@outlook.com>
2025-07-18 17:39:08 -0300

    routes from breeze registered, registering controllers index but keeping index out of the Auth middleware to allow for unauthenticated access

--- routes/web.php
+++ routes/web.php
@@ -19,4 +19,17 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::resource('contacts', ContactController::class)->except(['index']);
+Route::get('/contacts', [ ContactController::class,'index'])->name('contacts.index');
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    Route::resource('contacts', ContactController::class)->except(['index']);
+});
+
+require __DIR__.'/auth.php';
